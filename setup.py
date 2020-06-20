90f0f9bd7f37b6d63e41d1a9149a883fe6787f72
kirankotari <kirankotari@live.com>
2020-06-20 22:52:21 +0530

    removing core and reverting setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
+from setuptools import setup, find_packages
 from os import path
 from io import open
-from setuptools import setup, find_packages
-from ncs_pycli.core import release as rel
-
 
 here = path.abspath(path.dirname(__file__))
 reqs = []
@@ -10,26 +8,41 @@ reqs = []
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open(path.join(here, '.version'), encoding='utf-8') as f:
+    version = f.read()
+
 with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     read_lines = f.readlines()
     reqs = [each.strip() for each in read_lines]
 
 setup(
-    name           = rel.name,
-    url            = rel.url,
-    version        = rel.version,
-    description    = rel.description,
-    author         = rel.author,
-    author_email   = rel.author_email,
-    classifiers    = rel.classifiers,
-    keywords       = rel.keywords,
-    entry_point    = rel.entry_point if rel.entry else {},
-
-    install_requires = reqs,
+    name = 'ncs_pycli',
+    version = version,
+    description = "Gives you an interactive NSO python shell with tab completion.",
     long_description = long_description,
-    packages         = find_packages(where='.', exclude=['tests']),
-
-    include_package_data          = True,
     long_description_content_type = 'text/markdown',
+    url = 'https://github.com/NSO-developer/ncs_pycli.git',
+    author = 'Hakan Niska, Kiran Kumar Kotari',
+    author_email = 'hniska@cisco.com, kkotari@cisco.com',
+    entry_points={
+        'console_scripts': [
+            'ncs_pycli=ncs_pycli.ncs_pycli:run'
+        ],
+    },
+    install_requires=reqs,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ],
+    keywords = 'ncs-pycli ncs_pycli',
+    packages = find_packages(where='.', exclude=['tests']),
+    include_package_data=True,
 )
 
